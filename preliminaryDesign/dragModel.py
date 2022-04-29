9661d39b66ea5040951496a5167845948e336563
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-04-29 16:47:22 +0200

    added drag model

--- preliminaryDesign/dragModel.py
+++ preliminaryDesign/dragModel.py
@@ -1,2 +1,9 @@
 def dragModel(params, rho):
-    return None
+    wingDrag = 0.5 * rho * params['velocity'] ** 2 * \
+        (params['wingArea'] * params['wingC_D'])
+    balloonDrag = 0.5 * rho * params['velocity'] ** 2 * \
+        (params['balloonArea'] * params['balloonC_D'])
+    fuselageDrag = 0.5 * rho * \
+        params['velocity'] ** 2 * \
+        (params['fuselageArea'] * params['fuselageC_D'])
+    params['totalDrag'] = wingDrag + balloonDrag + fuselageDrag
