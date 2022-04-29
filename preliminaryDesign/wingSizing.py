4ea35c9a377f07aaa8330856c9fb21fc2cfafdb2
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-04-29 15:27:29 +0200

    made wing siiiiiiiiizeeeeee

--- preliminaryDesign/wingSizing.py
+++ preliminaryDesign/wingSizing.py
@@ -1,2 +1,26 @@
+from cgi import test
+
+
+g = 9.80665
+
+
 def wingSizing(params, rho):
-    return None
+    params["wingArea"] = params["totalMass"] * g * params["liftFactor"] * \
+        params["liftRatio"] / (0.5 * rho * params["velocity"]
+                               ** 2 * params["wingC_L_design"])
+
+    params["wingC_D"] = params['wingDragCorrection'] * params["wingC_D_0"]
+
+
+if __name__ == "__main__":
+    testDict = {
+        "totalMass": 10000,
+        "liftRatio": 0.5,
+        "wingC_L_design": 0.6,
+        "liftFactor": 1.1,
+        "wingDragCorrection": 1.1,
+        "wingC_D_0": 0.05,
+        "velocity": 140,
+    }
+    wingSizing(testDict, 1.225)
+    print(testDict)
