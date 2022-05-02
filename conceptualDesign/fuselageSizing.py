9b3217a69906055c1b274ae05e918c8ec76ba05d
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-05-02 09:51:40 +0200

    add calculation for the frontal area of the fusealge

--- conceptualDesign/fuselageSizing.py
+++ conceptualDesign/fuselageSizing.py
@@ -22,5 +22,8 @@ def fuselageSizing(params, dp):
 
     # https://en.wikipedia.org/wiki/Pressure_vessel
     # shape is a stadium of revolution
-    m_cabin = 2 * np.pi * r ** 2 * (l - r) * dp * rho_mat / sigma_mat * params["safetyFactor"]
+    m_cabin = 2 * np.pi * r ** 2 * \
+        (l - r) * dp * rho_mat / sigma_mat * params["safetyFactor"]
     print(m_cabin)
+
+    params["fuselageArea"] = np.pi * r ** 2
