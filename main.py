8fb1ed70597b5fd0b36106a87ca22453939e6b49
maxvanbart <maxvanbart@gmail.com>
2022-05-03 15:07:51 +0200

    minor changes

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     for i in range(100):
         params = parameters.copy()
         params["altitude"] = i * 100
-        params, _ = conceptualDesign(params, material_data)
+        params, _ = conceptualDesign(params, material_data, 50)
 
         lst.append(params["totalMass"])
 
