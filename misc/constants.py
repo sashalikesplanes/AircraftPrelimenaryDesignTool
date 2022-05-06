c9224c0beab8d9eecfdafcdd167c0f2767b69785
maxvanbart <maxvanbart@gmail.com>
2022-05-06 11:36:31 +0200

    unit test shizzle

--- misc/constants.py
+++ misc/constants.py
@@ -3,3 +3,4 @@ g = 9.80665  # m/s^2
 R_air = 287  # air gas constant [m^2/s^2/K]
 R_h2 = 4157.2  # h2 gas constant
 R = 8.31446261815324  # ideal gas constant [J/k/mol]
+testMargin = 0.02
---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+from misc.constants import testMargin
+
+from unit_test.testExample import TestExample
+
+
+def main():
+    """Main function for running unit tests"""
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
+
+# Run the followign two commands in order to generate a code coverage report.
+# coverage run -m unittest discover
+# coverage report
