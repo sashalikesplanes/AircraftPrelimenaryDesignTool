8289561f7a5922b8ec102a9ea8157d7a0b106e78
maxvanbart <maxvanbart@gmail.com>
2022-05-06 14:01:34 +0200

    couple of tests

--- conceptualDesign/fuelMassEstimation.py
+++ conceptualDesign/fuelMassEstimation.py
@@ -1,5 +1,4 @@
-# https://rmi.org/run-on-less-with-hydrogen-fuel-cells/#:~:text=In%20electrical%20terms%2C%20the%20energy,as%20a%20gallon%20of%20diesel.
-energyDensityHydrogen = 33.6e3 * 60 * 60  # J/kg
+from misc.constants import energyDensityHydrogen
 
 
 def fuelMassEstimation(params):
