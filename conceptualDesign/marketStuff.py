5484d395fa617e0df468cebc324cd0c47e52c341
tpotgieter <tpotgieter@student.tudelft.nl>
2022-05-16 10:31:52 +0200

    DOC, Aircraft cost, cost breakdown

--- conceptualDesign/marketStuff.py
+++ conceptualDesign/marketStuff.py
@@ -1,35 +1,39 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Personnel constants
 salaryPilot = 69  # [$/h]
 salaryAttendant = 38  # [$/h]
 salaryMaintenance = 50  # [$/h]
-crewCount = 5   # [-]
-annualAttendantSalary = 85e3   # [$]
-annualPilotSalary = 175e3       # [$]
+crewCount = 5  # [-]
+annualAttendantSalary = 85e3  # [$]
+annualPilotSalary = 175e3  # [$]
 # TODO: check this number and unit for cost burden
 costBurden = 2  # [$]
 
 # Operational constants
 h2Price = 2.5  # [$/kg]
-groundTime = 4  # [h]  --> refuelling (depends on fuel) + boarding payload
+groundTime = 10  # [h]  --> refuelling (depends on fuel) + boarding payload
 maintenanceTime = 2749  # [h]
 blockTimeSupplement = 1.8  # [h]
 f_atc = 0.7  # [-] 0.7 as ATC fees for transatlantic
 
 # CAPEX constants
-AircraftCost = 800e6    # [$] - should be changed
 
 # TODO: Revise CAPEX calculations to incorporate fuel tank costs properly
 # TODO: Do cost calculation for GH2 vs LH2
-P_OEW = 1200    # Price per operating empty weight [$/kg]
-P_eng = 2500    # price per engine weight
-W_eng = 2000
-IR = 0.05
-f_rv = 0.1
-f_ins = 0.005
-f_misc = 0.05  # TODO: Unknown correctness
-DP = 14
+P_OEW = 1200  # [$/kg] operating empty weight
+W_eng = 200  # Weight per engine
+P_eng = 2600  # [$/kg] engine
+P_fc = 608  # [$/kg] fuel cell
+P_tank = 475  # [$/kg] GH2
+
+IR = 0.05  # Interest rate
+f_rv = 0.1  # Residual value factor
+f_ins = 0.005  # Insurance cost
+f_misc = 0.05  # Misc contingency factor to account for new tech
+PMac = 0.2  # 20% profit margin for manufacturer
+DP = 14  # Depreciation period
 
 
 def marketStuff(params):
@@ -61,33 +65,43 @@ def marketStuff(params):
     # DOC maintenance
     DOC_maint_material = OEW / 1000 * (0.21 * blockTime + 13.7) + 57.5
     DOC_maint_personnel = salaryMaintenance * (1 + costBurden) * (
-                (0.655 + 0.01 * OEW / 1000) * blockTime + 0.254 + 0.01 * OEW / 1000)
+            (0.655 + 0.01 * OEW / 1000) * blockTime + 0.254 + 0.01 * OEW / 1000)
     tonForce = params["engineThrust"] * 0.0001124
     DOC_maint_engine = params["engineCount"] * (1.5 * tonForce + 30.5 * blockTime + 10.6)
     DOC_maintenance = flightCycles * (DOC_maint_engine + DOC_maint_material + DOC_maint_personnel)  # [$]
 
-    # DOC capex
-    a = IR * (1-f_rv * (1/(1+IR)) ** DP)/(1-(1/(1+IR)) ** DP)
-    DOC_cap = (P_OEW * (OEW - W_eng*params["engineCount"])+W_eng*params["engineCount"]*P_eng)*(a+f_ins)
+    # DOC capex (engines, fuel cell, fuel tak, cont. factor incl)
+    # TODO: Revise Pac calc. Should the other masses be subtracted from OEW?
+    a = IR * (1 - f_rv * (1 / (1 + IR)) ** DP) / (1 - (1 / (1 + IR)) ** DP)
+    Pac = (P_OEW * (OEW - W_eng * params["engineCount"]) + W_eng * params["engineCount"] * P_eng + params[
+        "tankMass"] * P_tank + params["fuelcellMass"] * P_fc) * (1 + PMac + f_misc)
+    DOC_cap = Pac * (a + f_ins)
 
-
-    # # TODO: finish capex stuff
-    # DOC_cap = AircraftCost  # [$]
-
-
+    # print(Pac)
 
     DOC = DOC_maintenance + DOC_crew + DOC_fees + DOC_fuel + DOC_cap  # [$]
     availableSeatKM = flightCycles * flightRange * params["passengers"] / 1000
     params["costPerPassengerKilometer"] = DOC / availableSeatKM  # [$/km/passenger]
 
     # Cost Breakdown [%]
-    frac_maintenance = DOC_maintenance / DOC
-    frac_crew = DOC_crew / DOC
-    frac_fees = DOC_fees / DOC
-    frac_fuel = DOC_fuel / DOC
-    frac_cap = DOC_cap / DOC
-    print(params["costPerPassengerKilometer"])
-    print(frac_maintenance, frac_crew, frac_fees, frac_fuel, frac_cap)
+    frac_maintenance = DOC_maintenance / DOC * 100
+    frac_crew = DOC_crew / DOC * 100
+    frac_fees = DOC_fees / DOC * 100
+    frac_fuel = DOC_fuel / DOC * 100
+    frac_cap = DOC_cap / DOC * 100
+    print(f"Aircraft Price [$]: {Pac}")
+    print(f"Direct Operating Cost [$]: {params['costPerPassengerKilometer']}")
+    print(f"Cost breakdown [%]: {frac_maintenance}, {frac_crew}, {frac_fees }, { frac_fuel}, {frac_cap}")
+
+    # Make pie chart
+    breakdown = np.array([frac_maintenance, frac_crew, frac_fees, frac_fuel, frac_cap])
+    mylabels = ["Maintenance", "Crew", "Fees", "Fuel", "CAPEX"]
+    colors = [plt.cm.Pastel1(i) for i in range(20)]
+    plt.pie(breakdown, labels=mylabels, autopct='%1.1f%%', colors=colors, startangle=90)
+    plt.title("Cost Breakdown [%]")
+    plt.axis('equal')
+    plt.savefig("..\plots\CostBreakdown")
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -100,7 +114,9 @@ if __name__ == "__main__":
         "pilotCount": 2,
         "passengers": 1000,
         "engineCount": 86,
-        "engineThrust": 6600  # power / velocity
+        "engineThrust": 6600,  # power / velocity
+        "tankMass": 100000,
+        "fuelcellMass": 20000
     }
 
     marketStuff(x)
