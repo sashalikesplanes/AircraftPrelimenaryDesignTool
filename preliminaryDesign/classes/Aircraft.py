e96cf3a5ddc3181dabcea9c14e6424579f520555
maxvanbart <maxvanbart@gmail.com>
2022-05-17 12:44:12 +0200

    start on fuselage sizing

--- preliminaryDesign/classes/Aircraft.py
+++ preliminaryDesign/classes/Aircraft.py
@@ -1,6 +1,21 @@
 import numpy as np
 
+from misc.openData import openData
+from preliminaryDesign.classes.Fuselage import Fuselage
+
 
 class Aircraft:
-    def __init__(self):
-        pass
+    def __init__(self, file):
+        data = openData(file)
+
+        # Cargo related data
+        self.passengers = data["passengers"]
+        self.cargoMass = data["cargoMass"]
+        self.passengerMass = data["passengerMass"]
+
+        # Components
+        self.fuselage = None
+
+    def makeFuselage(self):
+        self.fuselage = Fuselage()
+        self.fuselage.designCabin(self)
--- preliminaryDesign/classes/Fuselage.py
+++ preliminaryDesign/classes/Fuselage.py
@@ -1,6 +1,12 @@
 import numpy as np
 
+from preliminaryDesign.classes.Cabin import Cabin
+
 
 class Fuselage:
-    def __int__(self):
-        pass
+    def __init__(self):
+        self.cabin = None
+
+    def designCabin(self, plane):
+        self.cabin = Cabin(plane)
+
