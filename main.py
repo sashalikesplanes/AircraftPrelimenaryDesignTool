9253dfa07a8d0e52db94cce97c3ac5f9d43ed749
maxvanbart <maxvanbart@gmail.com>
2022-05-23 12:27:12 +0200

    added a cat

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from detailedDesign.detailDesign import detailDesign
 
 if __name__ == "__main__":
     detailDesign()
+
+
+#       /\_/\
+#  /\  / o o \
+# //\\ \~(*)~/
+# `  \/   ^ /
+#    | \|| ||  Lara
+#    \ '|| ||  Allen
+#     \)()-())
