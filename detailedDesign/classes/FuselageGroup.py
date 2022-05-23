d49ed02bd257aee662fb289657f5d4a6aadf98c5
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-05-23 10:44:05 +0200

    fixed imports

--- detailedDesign/classes/FuselageGroup.py
+++ detailedDesign/classes/FuselageGroup.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-from Component import Component
-from Tail import Tail
-from Power import Power
-=======
+from detailedDesign.classes.Tail import Tail
+from detailedDesign.classes.Power import Power
 # To Check
 from detailedDesign.classes.Component import Component
 
->>>>>>> d0ec95eb6639f204495a1bde9907d2436e8a906e
 
 class FuselageGroup(Component):
     def __init__(self, Aircraft):
