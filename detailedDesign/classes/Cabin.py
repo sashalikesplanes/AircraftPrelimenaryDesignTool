7922abcae9ab41ce4dbc70c98dbd269794f26046
maxvanbart <maxvanbart@gmail.com>
2022-05-23 11:45:18 +0200

    cleanup

--- detailedDesign/classes/Cabin.py
+++ detailedDesign/classes/Cabin.py
@@ -1,10 +1,7 @@
 from detailedDesign.classes.Component import Component
-# test
 
 
 class Cabin(Component):
     def __int__(self, Fuselage):
         super().__init__()
         self.Fuselage = Fuselage
-        # The bet for one beer
-        # hello cruel world
