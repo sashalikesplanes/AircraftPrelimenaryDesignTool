1b0cac8a491523bd47de4bac9b90aadbc4e92ed1
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-05-23 12:49:11 +0200

    added battery acid

--- detailedDesign/classes/Batteries.py
+++ detailedDesign/classes/Batteries.py
@@ -6,3 +6,30 @@ class Batteries(Component):
     def __init__(self, Power, config):
         my_config = super().__init__(config)
         self.Power = Power
+
+# Battery ACID ;)
+#                      H
+#                      |
+#                H  H  C--H
+#                 `.|,'|
+#                   C  H  H
+#                   |     |
+#              O    N  H  C
+#              \\ ,' `.|,'|`.
+#                C     C  H  H
+#                |     |
+#             H--C     H
+#              ,' `.
+#       H  H--C  H--C--H
+#       |     ||    |
+# H     C     C     N  H  H
+#  `. ,' `. ,' `. ,' `.|,'
+#    C  _  C  H  C     C
+#    | (_) |   `.|     |
+#    C     C     C     H
+#  ,' `. ,' `. ,' `.
+# H     C     C     H
+#       |    ||
+#       N-----C
+#       |     |
+#       H     H
