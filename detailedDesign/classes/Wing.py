c1420666720ca9af6e4fa89f0cdf246f9a66c57c
tpotgieter <tpotgieter@student.tudelft.nl>
2022-05-24 12:18:29 +0200

    Wing mass sizing

--- detailedDesign/classes/Wing.py
+++ detailedDesign/classes/Wing.py
@@ -1,8 +1,8 @@
 # To Check
 import numpy as np
-
 from detailedDesign.classes.Component import Component
 from detailedDesign.classes.HLDs import HLDs
+from misc.unitConversions import *
 
 
 class Wing(Component):
@@ -17,6 +17,7 @@ class Wing(Component):
         self.span = None
         self.tip_chord = None
         self.root_chord = None
+        self.sweep = None
 
         # Create all the parameters that this component must have here:
         # Using self.property_name = None
@@ -24,33 +25,48 @@ class Wing(Component):
 
     def size_AR(self):
         print(f'sizing the Aspect Ratio')
+        state = self.WingGroup.Aircraft.states["cruise"]
+
         Range = None
-        V_C = self.WingGroup.Aircraft.states['cruise'].velocity 
-        dynamic_pressure = 0.5 * self.WingGroup.Aircraft.states['cruise'].density * V_C * V_C
+
+        V_C = state.velocity
+        dynamic_pressure = 0.5 * state.density * V_C * V_C
         W_initial_cruise = None
         W_end_cruise = None
         C_L_initial_cruise = W_initial_cruise / (dynamic_pressure * self.wing_area)
         C_L_end_cruise = W_end_cruise / (dynamic_pressure * self.wing_area)
         C_LC = (C_L_intial_cruise + C_L_end_cruise) / 2
 
-
         C_D_min = aircraft.C_D_min
-        c_t_SI = self.WingGroup.Engines.thrust_specific_fuel_consumption #g/kNs
+        c_t_SI = self.WingGroup.Engines.thrust_specific_fuel_consumption  # g/kNs
         c_t_Imp = c_t_SI * 9.81 / 1e6 * 3600
 
-        optimal_effective_AR = C_LC*C_LC / np.pi / (V_C / Range \
-                * C_LC / c_t_Imp * np.log(W_intial_cruise \
-                / W_final_cruise) - C_D_min)
+        optimal_effective_AR = C_LC * C_LC / np.pi / (V_C / Range \
+                                                      * C_LC / c_t_Imp * np.log(W_intial_cruise \
+                                                                                / W_final_cruise) - C_D_min)
         print(optimal_effective_AR)
-
-
 
     def size_self(self):
         self.wing_area = self.WingGroup.Aircraft.reference_area
 
+        self.size_AR()
         self.span = (self.wing_area * self.aspect_ratio) ** 0.5
         # print(self.span)
         self.root_chord = (2 * self.wing_area) / (self.span * (1 + self.taper_ratio))
         self.tip_chord = self.root_chord * self.taper_ratio
-        self.size_AR()
+
+        self.sweep = 0  # M < 0.7
         # print(self.root_chord, self.tip_chord)
+
+        # Mass Sizing
+        state = self.WingGroup.Aircraft.states["cruise"]
+        q = pa_to_psi(0.5 * state.density * state.velocity ** 2)
+        S_W = m2_to_ft2(self.wing_area)
+        W_FW = 1  # Fuel in wings. There is no fuel in the wings therefore = 1
+        sweep = np.pi / 180. * self.sweep  # put it in radians
+        n_z = self.FuselageGroup.Aircraft.ultimate_load_factor
+        W_O = kg_to_lbs(self.FuselageGroup.Aircraft.mtom)
+
+        self.own_mass = 0.036 * S_W ** 0.758 * W_FW ** 0.0035 * (
+                    self.aspect_ratio / np.cos(sweep) ** 2) ** 0.6 * q ** 0.006 * self.taper_ratio ** 0.04 * (
+                                    (100 * self.thickness_chord_ratio) / np.cos(sweep)) ** (-0.3) * (n_z * W_O) ** 0.49
