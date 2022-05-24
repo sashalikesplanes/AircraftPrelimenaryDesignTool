929cf987e810b10ea55405d898a3c7e981b91b55
maxvanbart <maxvanbart@gmail.com>
2022-05-24 11:31:34 +0200

    emergency commit branch thing

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
 import unittest
 from misc.constants import testMargin
 
-# from unit_test.testExample import TestExample
-from unit_test.testBalloonSizing import TestBalloonSizing
-from unit_test.testEnergyRequired import TestEnergyRequired
-from unit_test.testFuelMassEstimation import TestFuelMassEstimation
-from unit_test.testFuselageSizing import TestFuselageSizing
-from unit_test.testInitializeParameters import initializeParameters
+from unit_test.testWeights import TestWeights
 from unit_test.testISA import TestISA
-from unit_test.testWingSizing import TestWingSizing
+
+# from unit_test.testExample import TestExample
 
 
 def main():
