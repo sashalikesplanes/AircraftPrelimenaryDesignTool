a998ca74de9aaeffbcf7ac3d929047cd2a82f097
katovanaken <vanaken.kato@gmail.com>
2022-05-24 12:08:50 +0200

    fuel tank

--- fueltank_test.py
+++ fueltank_test.py
@@ -3,16 +3,18 @@
 from pathlib import Path
 from misc.openData import openData
 from detailedDesign.classes.Aircraft import Aircraft
+from detailedDesign.classes.State import State
 
 
 def run_test():
     config_file = Path('data', 'new_designs', 'config.yaml')
-    aircraft = Aircraft(openData(config_file))
+    states = {"cruise": State('cruise')}
+    aircraft = Aircraft(openData(config_file), states)
 
     aircraft.reference_area = 10
 
     #init parameters
-    aircraft.FuselageGroup.Fuselage.inner_diameter = 1
+    aircraft.FuselageGroup.Fuselage.inner_diameter = None
 
     #call python file
     aircraft.FuselageGroup.Fuselage.FuelContainer.size_self()
