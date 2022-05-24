c98b4593fe3ea48e4986315fe8da757ad524c8d3
mikad74 <mika.doorenbosch@gmail.com>
2022-05-24 11:28:46 +0200

    take your hands of my test Potgieter

--- wing_test.py
+++ wing_test.py
@@ -3,11 +3,13 @@
 from pathlib import Path
 from misc.openData import openData
 from detailedDesign.classes.Aircraft import Aircraft
+from detailedDesign.classes.State import State
 
 
 def run_test():
     config_file = Path('data', 'new_designs', 'config.yaml')
-    aircraft = Aircraft(openData(config_file))
+    states = {"cruise": State('cruise')}
+    aircraft = Aircraft(openData(config_file), states)
 
     aircraft.reference_area = 10
 
