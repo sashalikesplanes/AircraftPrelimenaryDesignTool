d9885952021baea6ee3dbb547ee208682cb14b28
katovanaken <vanaken.kato@gmail.com>
2022-05-25 11:55:58 +0200

    fuel cell

---
+++ fuelcell_test.py
@@ -0,0 +1,18 @@
+
+from pathlib import Path
+from misc.openData import openData
+from detailedDesign.classes.Aircraft import Aircraft
+from detailedDesign.classes.State import State
+
+
+def run_test():
+    config_file = Path('data', 'new_designs', 'config.yaml')
+    states = {"cruise": State('cruise')}
+    aircraft = Aircraft(openData(config_file), states)
+
+    aircraft.reference_area = 10
+
+    aircraft.FuselageGroup.Power.FuelCells.size_self()
+
+if __name__ == "__main__":
+    run_test()
