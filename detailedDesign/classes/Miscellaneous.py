8f8ec345dcc9dbcad643b9c5c27324e895910773
maxvanbart <maxvanbart@gmail.com>
2022-05-25 12:11:46 +0200

    test miscellaneous weights completed

--- detailedDesign/classes/Miscellaneous.py
+++ detailedDesign/classes/Miscellaneous.py
@@ -8,6 +8,14 @@ class Miscellaneous(Component):
         super().__init__(design_config)
         self.FuselageGroup = FuselageGroup
 
+        self.W_boat = None
+        self.W_avionics = None
+        self.W_AC = None
+        self.W_electrical = None
+        self.W_furnishing = None
+        self.W_hydraulics = None
+        self.W_flight_control_system = None
+
         self._freeze()
 
     def size_self(self):
@@ -20,7 +28,7 @@ class Miscellaneous(Component):
         n_z = self.FuselageGroup.Aircraft.ultimate_load_factor  # [-]
         n_pax = self.FuselageGroup.Fuselage.Cabin.passengers  # [-]
         pilot_count = 3  # [-]
-        N_OCC = n_pax + n_pax % 50 + 1 + pilot_count  # [-]
+        N_OCC = n_pax + n_pax // 50 + 1 + pilot_count  # [-]
         # This needs to be in meters since the mach number requires metric units for speed of sound
         h = self.FuselageGroup.Aircraft.states["cruise"].altitude  # [m]
         M = self.FuselageGroup.Aircraft.states["cruise"].velocity / getSpeedOfSound(h)  # [-]
@@ -30,20 +38,19 @@ class Miscellaneous(Component):
         W_UAV = 420  # [lbs]
 
         # MAGICAL DISNEY FACTOR (MDF)
-        W_boat = self.FuselageGroup.Fuselage.get_mass() * 0.2
+        self.W_boat = self.FuselageGroup.Fuselage.get_mass() * 0.2
 
-        W_flight_control_system = 0.053 * l_FS ** 1.536 * b ** 0.371 * (n_z * W_O * 10 ** -4) ** 0.8
+        self.W_flight_control_system = lbs_to_kg(0.053 * l_FS ** 1.536 * b ** 0.371 * (n_z * W_O * 10 ** -4) ** 0.8)
 
-        W_hydraulics = 0.001 * W_O
+        self.W_hydraulics = lbs_to_kg(0.001 * W_O)
 
-        W_avionics = 2.117 * W_UAV ** 0.933
+        self.W_avionics = lbs_to_kg(2.117 * W_UAV ** 0.933)
 
-        W_electrical = 12.57 * (W_FS + W_avionics) ** 0.51
+        self.W_electrical = lbs_to_kg(12.57 * (W_FS + kg_to_lbs(self.W_avionics)) ** 0.51)
 
-        W_AC = 0.265 * W_O ** 0.52 * N_OCC ** 0.68 * W_avionics ** 0.17 * M ** 0.08
+        self.W_AC = lbs_to_kg(0.265 * W_O ** 0.52 * N_OCC ** 0.68 * kg_to_lbs(self.W_avionics) ** 0.17 * M ** 0.08)
 
-        W_furnishing = 0.0582 * W_O - 65
+        self.W_furnishing = lbs_to_kg(0.0582 * W_O - 65)
 
-        mass = W_furnishing + W_AC + W_electrical + W_avionics + W_hydraulics + W_flight_control_system + W_boat
-        mass = lbs_to_kg(mass)
+        mass = self.W_furnishing + self.W_AC + self.W_electrical + self.W_avionics + self.W_hydraulics + self.W_flight_control_system + self.W_boat
         self.own_mass = mass
