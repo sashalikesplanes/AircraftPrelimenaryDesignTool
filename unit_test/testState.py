037b5a039c1d173054cc4f8e491ebed36a9b2005
maxvanbart <maxvanbart@gmail.com>
2022-05-25 12:26:14 +0200

    fix a thing

--- unit_test/testState.py
+++ unit_test/testState.py
@@ -8,14 +8,18 @@ class TestState(unittest.TestCase):
     def test_stationary_state(self):
         self.state = State("test_state_1")
 
-        self.assertEqual(self.state.velocity, 0)
-        self.assertEqual(self.state.altitude, 0)
+        self.assertEqual(self.state.velocity, 10)
+        self.assertEqual(self.state.altitude, 1000)
         self.assertEqual(self.state.range, 100000)
 
-        self.assertEqual(self.state.pressure, 101325)
-        self.assertEqual(self.state.temperature, 15 + 273.15)
-        self.assertEqual(self.state.density, 1.225)
-        self.assertAlmostEqual(self.state.speed_of_sound, 340, delta=340*testMargin)
+        y = 89874.6
+        self.assertAlmostEqual(self.state.pressure, y, delta=y*testMargin)
+        y = 281.650
+        self.assertAlmostEqual(self.state.temperature, y, delta=y*testMargin)
+        y = 1.11164
+        self.assertAlmostEqual(self.state.density, y, delta=y*testMargin)
+        y = 336.434
+        self.assertAlmostEqual(self.state.speed_of_sound, y, delta=y*testMargin)
 
     def test_high_place_state(self):
         self.state = State("test_state_2")
