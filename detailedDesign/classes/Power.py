14ccc712dc5cf74afa5e971f00d7ce40148ef099
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-05-25 16:15:53 +0200

    changed git_sized in Power to be correct

--- detailedDesign/classes/Power.py
+++ detailedDesign/classes/Power.py
@@ -4,6 +4,8 @@ from detailedDesign.classes.FuelCells import FuelCells
 from detailedDesign.classes.Batteries import Batteries
 import numpy as np
 
+from detailedDesign.classes.FuselageGroup import FuselageGroup
+
 
 class Power(Component):
     def __init__(self, FuselageGroup, design_config):
@@ -21,7 +23,7 @@ class Power(Component):
         self._freeze()
 
     def size_self(self):
-        P_motor = Engines.P_motor
+        P_motor = FuselageGroup.Aircraft.WingGroup.Engines.P_motor
         n_motor = self.FuselageGroup.Aircraft.WingGroup.Engines.amount_motor
         percent_prop = self.percentage_propulsion_power
         eff_inverter = self.eff_inverter
@@ -29,10 +31,16 @@ class Power(Component):
         cable_contingency = self.cable_contingency
 
         # necessary power output from fuel cells
-        P_output_fuelcells = (n_motor * P_motor) / (percent_prop * eff_inverter * eff_converter)
+        P_output_fuelcells = (n_motor * P_motor) / \
+            (percent_prop * eff_inverter * eff_converter)
 
         # taking into account cable losses, thus required power from fuel cells
         P_required = P_output_fuelcells * cable_contingency
 
         self.own_power = P_required
 
+    def get_sized(self):
+        for component in self.components:
+            component.get_sized()
+
+        self.own_mass = self.get_mass()
