a82068ad5528bbfb8cb173a96e0fc8833c3e02e7
tpotgieter <tpotgieter@student.tudelft.nl>
2022-05-25 16:15:52 +0200

    fix main

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from detailedDesign.detailDesign import detail_design
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if __name__ == "__main__":
+    detail_design()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#       /\_/\
+#  /\  / o o \
+# //\\ \~(*)~/
+# `  \/   ^ /
+#    | \|| ||  Lara
+#    \ '|| ||  Allen
+#     \)()-())
+# The cat ascii art of truth
