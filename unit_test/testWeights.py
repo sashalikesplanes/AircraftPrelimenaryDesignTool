2cfe1d2500a594a4aa068ff983ead87be533f02c
tpotgieter <tpotgieter@student.tudelft.nl>
2022-05-25 12:15:31 +0200

    Unit tests and other stuff

--- unit_test/testWeights.py
+++ unit_test/testWeights.py
@@ -2,6 +2,8 @@ import unittest
 from pathlib import Path
 from misc.constants import testMargin
 from misc.openData import openData
+from misc.unitConversions import *
+from misc.ISA import *
 from detailedDesign.classes.State import State
 from detailedDesign.classes.Aircraft import Aircraft
 
@@ -17,7 +19,7 @@ class TestWeights(unittest.TestCase):
         # Define test params
         self.aircraft.FuselageGroup.Fuselage.Cabin.diameter = 10  # [m]
         self.aircraft.FuselageGroup.Fuselage.Cabin.length = 100  # [m]
-        self.aircraft.FuselageGroup.Fuselage.Cabin.cabin_pressure_altitude = 1000  # [m]
+        self.aircraft.FuselageGroup.Fuselage.Cabin.cabin_pressure_altitude = 2000  # [m]
         self.aircraft.FuselageGroup.Fuselage.Cabin.passengers = 250  # [-]
 
         self.aircraft.FuselageGroup.Fuselage.FuelContainer.length = 50  # [m]
@@ -34,13 +36,41 @@ class TestWeights(unittest.TestCase):
         self.aircraft.WingGroup.Wing.thickness_chord_ratio = 0.1  # [-]
 
 
-    def test_fuselage_mass(self):
-        # Test
-        self.aircraft.FuselageGroup.Fuselage.size_self()
-        x = self.aircraft.FuselageGroup.Fuselage.own_mass
-        y = 32452345
-        self.assertAlmostEqual(x, y, delta=0.3 * testMargin)
+        # Imperial constants from test params:
+        # l_FS = 492.12598 # [ft]
+        # S_fus = 55308.51562 # [ft2]
+        # W_o = 220462.2622   # [lbs]
+        # l_HT = 270.74245    # [ft]
+        # d_FS = 32.8084      # [ft]
+        # q = 0.0080615       # [psi] for test_state_1
+        # V_p = 277360.9475   # [ft3] for test_state_1
+        # Delta_P = 0         # [psi] for test_state_1
 
+    def test_fuselage_mass(self):
+        self.aircraft.FuselageGroup.Fuselage.size_self()
+        state = self.aircraft.FuselageGroup.Aircraft.states["cruise"]
+
+        # mass test
+        model_mass = self.aircraft.FuselageGroup.Fuselage.own_mass
+
+        # Adapting values for changing cruise state
+        q = pa_to_psf(0.5 * state.density * state.velocity ** 2)  # [psi]
+        Delta_P = pa_to_psi(
+            getPressure(self.aircraft.FuselageGroup.Fuselage.Cabin.cabin_pressure_altitude) - state.pressure)  # [psi]
+        if Delta_P < 0:
+            Delta_P = 0
+
+        analytical_mass_lbs = 0.052 * 55308.51562 ** 1.086 * 440924.5244 ** 0.177 * 270.74245 ** -0.051 * (
+                    492.12598 / 32.8084) ** (
+                                  -0.072) * q ** 0.241 + 11.9 * (277360.9475 * Delta_P) ** 0.271
+
+        analytical_mass_kg = lbs_to_kg(analytical_mass_lbs)
+
+        # analytical_mass_kg = 6443.190189 # test_state_1
+
+        print(f"Mass of fuselage: {model_mass} [kg]")
+
+        self.assertAlmostEqual(model_mass, analytical_mass_kg, delta=analytical_mass_kg * testMargin)
 
     def test_wing_mass(self):
         # Define params
@@ -49,8 +79,6 @@ class TestWeights(unittest.TestCase):
         y = 1
         self.assertAlmostEqual(x,y, delta= 0.3* testMargin)
 
-
-
     def test_horizontal_tail_mass(self):
         pass
 
@@ -58,11 +86,9 @@ class TestWeights(unittest.TestCase):
         pass
 
     def test_misc_mass(self):
-        print(self.aircraft.FuselageGroup.Fuselage.Cabin.diameter)
-
         self.aircraft.FuselageGroup.Miscellaneous.size_self()
         x = self.aircraft.FuselageGroup.Miscellaneous.own_mass
-        print(f"Mass of miscellaneous group: {x}")
+        print(f"Mass of miscellaneous group: {x} [kg]")
+
         y = 0
         self.assertAlmostEqual(x, y, y * testMargin)
-
