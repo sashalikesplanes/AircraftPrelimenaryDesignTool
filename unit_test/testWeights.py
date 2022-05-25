a05a2ea9ef9d0c7588b5e6af140968c58d741f4d
tpotgieter <tpotgieter@student.tudelft.nl>
2022-05-25 10:06:19 +0200

    Unit tests and other stuff

--- unit_test/testWeights.py
+++ unit_test/testWeights.py
@@ -10,14 +10,14 @@ class TestWeights(unittest.TestCase):
 
     def setUp(self):
         config_file = Path('data', 'new_designs', 'config.yaml')
-        states = {"stationary": State('stationary')}
+        states = {"test_state_1": State('test_state_1')}
         self.aircraft = Aircraft(openData(config_file), states)
         # self.aircraft.FuselageGroup.get_sized()
 
-        # Define params
+        # Define test params
         self.aircraft.FuselageGroup.Fuselage.Cabin.diameter = 10  # [m]
         self.aircraft.FuselageGroup.Fuselage.FuelContainer.length = 50  # [m]
-        self.aircraft.FuselageGroup.Fuselage.Cabin.length = 100
+        self.aircraft.FuselageGroup.Fuselage.Cabin.length = 100         # [m]
         self.aircraft.FuselageGroup.Aircraft.ultimate_load_factor = 2  # [-]
         self.aircraft.FuselageGroup.Aircraft.mtom = 100000  # [kg]
         self.aircraft.FuselageGroup.Fuselage.Cabin.cabin_pressure_altitude = 1000  # [m]
@@ -33,7 +33,7 @@ class TestWeights(unittest.TestCase):
 
     def test_wing_mass(self):
         # Define params
-
+        pass
 
 
 
