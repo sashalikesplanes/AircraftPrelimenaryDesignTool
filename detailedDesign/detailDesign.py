15c4d80397e85872687ef3fbbd94b2c5a92f4a84
maxvanbart <maxvanbart@gmail.com>
2022-06-01 12:46:42 +0200

    move sketch to other file

--- detailedDesign/detailDesign.py
+++ detailedDesign/detailDesign.py
@@ -11,6 +11,7 @@ from detailedDesign.historicalRelations import get_MTOM_from_historical_relation
 from detailedDesign.log import setup_custom_logger
 from detailedDesign.potatoPlot import make_potato_plot
 from detailedDesign.flightEnvelope import make_flight_envelope
+from detailedDesign.sketch import sketch_aircraft
 
 
 def get_ultimate_load_factor():
@@ -63,44 +64,7 @@ def detail_design(debug=False):
     plt.ylabel("Maximum take-off mass [kg]")
     plt.title("MTOM over iterations")
 
-    print("Plotting things")
-    plt.figure(2)
-    things = aircraft.plot_cgs()
-    print(things)
-    for thing in things:
-        plt.scatter(thing[0][0], thing[0][2])
-        plt.annotate(thing[1], (thing[0][0], thing[0][2]))
-    plt.title("CG positions relative to nose")
-    plt.xlabel("Longitudanal postion [m]")
-    plt.ylabel("Height position [m]")
-
-    # plot fuselage
-    length = aircraft.FuselageGroup.Fuselage.length
-    diameter = aircraft.FuselageGroup.Fuselage.outer_diameter
-    radius = diameter / 2
-    print(f"Slenderness ratio: {length/diameter}")
-    x = [0, 0, length, length, 0]
-    y = [radius, -radius, -radius, radius, radius]
-    plt.plot(x, y)
-    # plot mac
-    offset = aircraft.WingGroup.pos
-    mac = aircraft.WingGroup.Wing.mean_geometric_chord
-    thiccness = aircraft.WingGroup.Wing.thickness_chord_ratio * mac / 2
-    y = np.array([thiccness, -thiccness, -thiccness, thiccness, thiccness]) + offset[2]
-    x = np.array([0, 0, mac, mac, 0]) + offset[0]
-    plt.plot(x, y)
-
-    # vertical tail
-    offset = aircraft.FuselageGroup.Tail.pos
-    thiccness = aircraft.FuselageGroup.Tail.VerticalTail.span
-    mac = aircraft.FuselageGroup.Tail.VerticalTail.mean_geometric_chord
-    y = np.array([0, -thiccness, -thiccness, 0, 0]) + offset[2]
-    x = np.array([0, 0, -mac, -mac, 0]) + offset[0]
-    plt.plot(x, y)
-
-    plt.gca().invert_yaxis()
-    plt.show()
-    print("Plotted things")
+    sketch_aircraft(aircraft)
 
     print(f"Aircraft CG: {aircraft.get_cg()}")
 
