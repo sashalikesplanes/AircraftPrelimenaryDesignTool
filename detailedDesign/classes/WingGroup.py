a052880514bc30924120738b0752d23f31a170c1
atzeh <a.j.harmsen@student.tudelft.nl>
2022-06-01 11:47:44 +0200

    made winggroup cg

--- detailedDesign/classes/WingGroup.py
+++ detailedDesign/classes/WingGroup.py
@@ -23,6 +23,7 @@ class WingGroup(Component):
             component.get_sized()
 
             self.logger.debug(f"Mass of {type(component).__name__} : {component.get_mass():.4E} kg")
+        self.size_self()
 
     def size_self(self):
         self.pos = np.array([self.Aircraft.x_lemac, 0., -self.Aircraft.FuselageGroup.Fuselage.outer_diameter/2])
