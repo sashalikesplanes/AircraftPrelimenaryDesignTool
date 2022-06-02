28e5715a0208f041389624004495265dd6b53121
ddiazgarcia <ddiazgarcia@student.tudelft.nl>
2022-06-02 16:51:46 +0200

    fan_sizing

--- P&P/Fan_sizing.py
+++ P&P/Fan_sizing.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-pr = 1.5           # pressure ratio
+pr = 1.3           # pressure ratio
 flow_coef = 0.5     # whittle fan
 omega = 2500 * 2* np.pi / 60  # rpm of the emotor from 2500 to 1800
 
@@ -10,7 +10,7 @@ rho =  0.8194       # kg/m3 for 4000 m
 Ps = 61640.24       # Pa static pressure at 4000 m
 #Ps = 16157.9       #A380
 
-Tt = 659256.83
+Tt = 350000000 # N
 #Tt = 53500 * 4.44822 * 4  # from propeller sizing example
 #Tt = 4 * 70000 * 4.44822 # N A380
 
@@ -28,27 +28,29 @@ Pt0 = Ps + 0.5 * rho * V0**2
 Pt1 = Pt0 * pr
 V1 = np.sqrt(2*(Pt1 - Ps) / rho)
 m_dot = Tf / (V1-V0)
-r = (m_dot/ (flow_coef * rho * np.pi * omega)) ** (1/3)
+r = (m_dot/ (flow_coef * rho * np.pi * omega)) ** (1/3) #
 
 # get the weight of ducted fan
 D_fan = 2*r
-mass_fan = 389.54*D_fan**2 + 55.431*D_fan - 2.064  # from excel regression
-vol_fan = 4.9804*D_fan**2 - 4.6767*D_fan + 1.3557
+mass_fan = 259.69*D_fan**2 + 36.954*D_fan - 1.376    # 1/3
+#mass_fan = 389.54*D_fan**2 + 55.431*D_fan - 2.064  # 1/2
+vol_fan = 3.3203*D_fan**2 - 3.1178*D_fan + 0.9038                # 1/3
+#vol_fan = 4.9804*D_fan**2 - 4.6767*D_fan + 1.3557   # 1/2
 length_fan  = vol_fan/(np.pi*r**2)
 
 # get weight motor + inverter
 #mass_motor_inverter =       # from email saluqi
-specific_mass = 10000  # [w/kg] from email of saluqi
-mass_motor_inverter = P_motor / specific_mass
+specific_mass_motor_inverter = 10000  # [w/kg] from email of saluqi
+mass_motor_inverter = P_motor / specific_mass_motor_inverter
 
 # total weight of prop subsys
-mass_total = n_fans * ( mass_fan + mass_motor_inverter)  # gearbox??
+mass_total = n_fans * ( mass_fan + mass_motor_inverter)  #
 
 # spacing
 Span = 120
-D_fus = 9
+D_fus = 8.5
 length_ailerons = 0.2* Span  # the aileron are 20 % of the total span
-min_spacing = 0.1
+min_spacing = 0.07 * D_fan
 spacing = (Span - D_fus - 2*length_ailerons -2*min_spacing - D_fan* n_fans)  / (n_fans-2)
 if 0<spacing< min_spacing :
     n_fans_fit = (Span - D_fus - 2*length_ailerons)/(D_fan+min_spacing)
@@ -58,7 +60,7 @@ elif spacing<0 :
     print('The engines do not fit on the wing. There are', n_fans,'fans but only', n_fans_fit, 'fit in the wing')
 
 
-print ('Radius of a fan', r,'m')
+print ('Diameter of a fan', D_fan,'m')
 print ('Number of fans', n_fans)
 print ('Blade tip speed', omega*r, 'm/s')
 print ('Length of fan', length_fan, 'm')
--- detailedDesign/classes/Engines.py
+++ detailedDesign/classes/Engines.py
@@ -1,7 +1,7 @@
 # To Check
 from detailedDesign.classes.Component import Component
 import numpy as np
-
+import misc.ISA as ISA
 
 class Engines(Component):
     def __init__(self, WingGroup, design_config):
@@ -11,99 +11,97 @@ class Engines(Component):
 
         # Create all the parameters that this component must have here:
         # Using self.property_name = value
-        self.volume = 0
-        self.own_total_mass = 0
-        self.own_amount_prop = 0
+        self.own_mass = 0
+        self.own_amount_fans = 0
         self.own_lenght_unit = 0
-        self.own_width_unit = 0
-        self.own_height_unit = 0
-        self.own_amount_motor = None
-
+        self.own_diameter_fan = 0
+        self.own_spacing = 0
+        self.own_fans_on_wing = 0
+        self.own_fans_on_fuselage = 0
         self._freeze()
 
     @property
     def amount_motor(self):
-        return self.own_amount_motor
+        return self.own_amount_fans
 
     def size_self(self):
-        S = self.WingGroup.Wing.span
-        V = self.WingGroup.Aircraft.states['cruise'].velocity
-        range_ = self.WingGroup.Aircraft.states['cruise'].range
-        T = self.WingGroup.Aircraft.reference_thrust
+        Span = self.WingGroup.Wing.span
+        V0 = self.WingGroup.Aircraft.states['cruise'].velocity
+        altitude = self.WingGroup.Aircraft.states['cruise'].altitude
+        Tt = self.WingGroup.Aircraft.reference_thrust
         D_fus = self.WingGroup.Aircraft.FuselageGroup.Fuselage.outer_width
-        self.logger.debug(f"{ range_ = }, { V = }")
-        # Ref aircraft An-22 data
-        P_eng = self.P_eng_an22  # [W] Power of one engine of the An-22
-        D_prop_an22 = self.D_prop_an22  # [m] diameter of propeller of An-22
+
 
         # Constants
         P_motor = self.P_motor  # power of the electric motor [W]
-        m_specific_motor = self.m_specific_motor  # [W/kg] for the 2MW motor
-        # [m] diameter of the 2MW motor using the coca-cola method
-        d_motor = self.d_motor
-        l_motor = self.l_motor  # [m] length of a motor
-        clearance = self.clearance  # distance between fuselage and engine from literature
-        m_propeller = self.m_propellor  # [kg] from the excel extrapolation
-        l_inverter = self.l_inverter
-        m_specific_inverter = self.m_specific_inverter
-        vol_specific_inverter = self.vol_specific_inverter
+        specific_mass_motor_inverter = self.specific_mass_motor_inverter  # [W/kg] for the 2MW motor
+        omega = self.rotational_speed_emotor
+        pr = self.pressure_ratio
+        flow_coef = self.flow_coef
+        eff_mot_inv = self.eff_mot_inv
+        propulsive_eff = self.propulsive_eff
+        increase_BLI_eff = self.increase_BLI_eff
+        pylon_mass_contingency = self.pylon_mass_contingency
+        rho = ISA.getDensity(altitude)
+        Ps = ISA.getPressure(altitude)
 
-        # Contingencies and efficiencies
-        eff_gearbox = self.eff_gearbox  # efficiency of a gearbox connection
-        spacing_motor_contingency = self.spacing_motor_contingency
-        height_unit_contingency = self.height_unit_contingency
-        gearbox_unit_vol_contingency = self.gearbox_unit_vol_contingency
-        gearbox_unit_mass_contingency = self.gearbox_unit_mass_contingency
-        width_gearbox_contingency = self.width_gearbox_contingency
+        ##### TODO
+        length_ailerons = 0.2 * Span  # the aileron are 20 % of the total span
 
         # Calculations
-        P_aircraft = T * V  # power the aircraft needs  [W]
-        # amount of motors per propellor
-        group = np.ceil(P_eng / (P_motor * eff_gearbox))
-        n_prop = np.ceil(P_aircraft / P_eng)
-        n_motor = group * n_prop
-        # n_prop = np.ceil((P_aircraft/P_eng)/2)*2    # we want an even number of propelors
-        # n_motor = np.ceil((group * n_prop)/2)*2
+        P_aircraft = Tt * V0
+        n_fans = np.ceil(P_aircraft / (P_motor * eff_mot_inv * (propulsive_eff+increase_BLI_eff)))
+        Tf = Tt / n_fans
+        Pt0 = Ps + 0.5 * rho * V0 ** 2
+        Pt1 = Pt0 * pr
+        V1 = np.sqrt(2 * (Pt1 - Ps) / rho)
+        m_dot = Tf / (V1 - V0)
+        r = (m_dot / (flow_coef * rho * np.pi * omega)) ** (1 / 3)
+        D_fan = 2 * r
+        min_spacing = 0.07 * D_fan
 
-        length_unit = np.ceil(
-            group / 2) * d_motor * spacing_motor_contingency  # [m] asuming they are connected in series
-        width_unit = 2 * (l_motor + l_inverter) * width_gearbox_contingency
-        height_unit = d_motor * height_unit_contingency
+        # get the weight of ducted fan
+        mass_fan = 389.54 * D_fan ** 2 + 55.431 * D_fan - 2.064  # from excel regression
+        vol_fan = 4.9804 * D_fan ** 2 - 4.6767 * D_fan + 1.3557
+        length_fan = vol_fan / (np.pi * r ** 2)
 
-        S_fit = (n_prop-1) * D_prop_an22 + 2 * clearance + (1 / 3) * D_prop_an22 * (
-            n_prop - 2) + D_fus  # ideal span to fit all propellers needed
-        n_prop_fit = np.floor((S - 2 * clearance - D_fus + (2 / 3) * D_prop_an22) / (
-            (4 / 3) * D_prop_an22))  # amount of propellers needed to fit in the desired span
+        # TODO get weight motor + inverter
+        # mass_motor_inverter =       # from email saluqi
+        mass_motor_inverter = P_motor / specific_mass_motor_inverter
 
-        if S_fit > S:
-            self.logger.warning(
-                f"The propellers don't fit in the Span. The span should be at least {S_fit} but it is {S}. The maximum amount of propellors that fit is {n_prop_fit}")
+        # total weight of prop subsys
+        mass_total = n_fans * (mass_fan + mass_motor_inverter) * pylon_mass_contingency # gearbox??
 
-        d_max_prop = (S - D_fus - 2 * clearance) / (
-            (4 / 3) * n_prop - (2 / 3))  # from the geometry with spacing of 1/3 of d_prop
+        # spacing
+        if (n_fans % 2) == 0:
+            n_fans_wing = n_fans
+            n_fans_odd = 0
+        elif (n_fans % 2) == 1:
+            n_fans_wing = n_fans -1
+            n_fans_odd = 1
+        spacing = (Span - D_fus - 2 * length_ailerons - 2 * min_spacing - D_fan * n_fans_wing) / (n_fans_wing - 2)
+        if 0 <= spacing < min_spacing:
+            n_fans_fit_wing = (Span - D_fus - 2 * length_ailerons) / (D_fan + min_spacing)
+            n_fans_fuselage = n_fans - n_fans_fit_wing
+            self.logger.warning(f"The engines are too close together. There are {n_fans} fans but only {n_fans_fit_wing} fit in the wing. "
+                                f"The number of fans on the fuselage is {n_fans_fuselage}")
+        elif spacing < 0:
+            n_fans_fit_wing = (Span - D_fus - 2 * length_ailerons) / (D_fan + min_spacing)
+            n_fans_fuselage = n_fans - n_fans_fit_wing
+            self.logger.warning(f"The engines do not fit on the wing. There are {n_fans} fans but only {n_fans_fit_wing} fit in the wing"
+                                f"The number of fans on the fuselage is {n_fans_fuselage}")
+        else:
+            n_fans_fit_wing = n_fans_wing
+            n_fans_fuselage = n_fans_odd
 
-        # mass of the motors needed to produce the thrust
-        m_motor = P_motor / m_specific_motor
-        vol_motor = d_motor * d_motor * l_motor  # [m3]
-        # the volume is calculated as if the motor had a box like shape
-        # instead of being a cylinder
 
-        m_inverter = P_motor / m_specific_inverter
-        vol_inverter = P_motor / vol_specific_inverter
-
-        vol_unit = group * (vol_motor + vol_inverter) * \
-            gearbox_unit_vol_contingency
-        vol_total = n_prop * vol_unit
-        m_unit = ((m_motor + m_inverter) * group + m_propeller) * \
-            gearbox_unit_mass_contingency
-        m_total = n_prop * m_unit
-
-        self.own_mass = m_total
-        self.own_amount_prop = n_prop
-        self.own_lenght_unit = length_unit
-        self.own_width_unit = width_unit
-        self.own_height_unit = height_unit
-        self.own_amount_motor = n_motor
+        self.own_mass = mass_total
+        self.own_amount_fans = n_fans
+        self.own_lenght_unit = length_fan
+        self.own_diameter_fan = D_fan
+        self.own_spacing = spacing
+        self.own_fans_on_wing = n_fans_fit_wing
+        self.own_fans_on_fuselage = n_fans_fuselage
         self.pos = np.array([0., 0., 0.])
 
     def cg_self(self):
