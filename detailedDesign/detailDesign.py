071342c187fd77aca0ee340a0827f8e7c185d10c
maxvanbart <maxvanbart@gmail.com>
2022-06-02 14:54:13 +0200

    potato 8: the last tuber

--- detailedDesign/detailDesign.py
+++ detailedDesign/detailDesign.py
@@ -1,15 +1,11 @@
-import numpy as np
 from pathlib import Path
 from matplotlib import pyplot as plt
 
 from misc.openData import openData
 from detailedDesign.classes.Aircraft import Aircraft
 from detailedDesign.classes.State import State
-from detailedDesign.potatoPlot import make_potato_plot
-from detailedDesign.sketch import sketch_aircraft
 from detailedDesign.run_aircraft import run_aircraft
-from detailedDesign.carrotPlot import make_carrot_plot
-from detailedDesign.classes.Stability_draft import get_xplot
+from detailedDesign.performAnalyses import perform_analyses
 
 
 # def get_ultimate_load_factor():
@@ -26,45 +22,13 @@ def detail_design(debug=False):
     aircraft = Aircraft(openData(config_file), states, debug=debug)
 
     run_aircraft(aircraft, debug=debug)
-    print("DONE")
 
     # ########################################### #
     # ##   MAKE ALL THE COOL PLOTS AND STUFF   ## #
     # ########################################### #
-    # sketch_aircraft(aircraft)
-    # make_potato_plot(aircraft, debug=debug)
-    # print(f"Aircraft CG: {aircraft.get_cg()}")
-    # perform_analyses(aircraft)
+
+    perform_analyses(aircraft)
+
     # make_flight_envelope(aircraft, "cruise")
     # make_flight_envelope(aircraft, "take-off")
     # aircraft.WingGroup.Wing.size_AR(aircraft)
-    # make_potato_plot(aircraft)
-
-    df = make_carrot_plot()
-    f_stab, f_cont = get_xplot(aircraft)
-
-    plt.figure(5)
-    y_min = 1e6
-    z_min = None
-    for row in np.array(df):
-        z = row[1]
-        x1 = row[2]
-        x2 = row[3]
-        y1 = f_cont(row[2])
-        y2 = f_stab(row[3])
-        if y1 > y2:
-            y = y1
-        else:
-            y = y2
-
-        if y < y_min:
-            y_min = y
-            z_min = z
-        plt.plot([x1, x2], [y, y], "--")
-
-    print(f"Best Sh/S: {y_min}, best Xlemac/Lfus: {z_min}")
-    plt.show()
-
-
-if __name__ == "__main__":
-    detail_design(debug=True)
