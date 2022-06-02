23a49c4ce1cff2704267bb6c9ffa19d29279d649
ddiazgarcia <ddiazgarcia@student.tudelft.nl>
2022-06-02 11:44:54 +0200

    fan_sizing

--- P&P/Fan_sizing.py
+++ P&P/Fan_sizing.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-pr = 1.9           # pressure ratio
+pr = 1.5           # pressure ratio
 flow_coef = 0.5     # whittle fan
 omega = 2500 * 2* np.pi / 60  # rpm of the emotor from 2500 to 1800
 
@@ -21,27 +21,48 @@ P_motor = 4000000   # W
 eff_mot_inv = 0.95  #
 
 P_aircraft = Tt * V0
-n_fans = P_aircraft/(P_motor * eff_mot_inv)
+n_fans = np.ceil(P_aircraft/(P_motor * eff_mot_inv))
 
 Tf = Tt/n_fans
 Pt0 = Ps + 0.5 * rho * V0**2
 Pt1 = Pt0 * pr
 V1 = np.sqrt(2*(Pt1 - Ps) / rho)
 m_dot = Tf / (V1-V0)
-
-# Ut = omega * r
-# A = m_dot / (flow_coef * rho * Ut)
-# A = np.pi * r
 r = (m_dot/ (flow_coef * rho * np.pi * omega)) ** (1/3)
 
-
 # get the weight of ducted fan
-d = 2*r
-weight = 0.0004*d**2 + 0.0554*d - 2.064
+D_fan = 2*r
+mass_fan = 389.54*D_fan**2 + 55.431*D_fan - 2.064  # from excel regression
+vol_fan = 4.9804*D_fan**2 - 4.6767*D_fan + 1.3557
+length_fan  = vol_fan/(np.pi*r**2)
+
+# get weight motor + inverter
+#mass_motor_inverter =       # from email saluqi
+specific_mass = 10000  # [w/kg] from email of saluqi
+mass_motor_inverter = P_motor / specific_mass
+
+# total weight of prop subsys
+mass_total = n_fans * ( mass_fan + mass_motor_inverter)  # gearbox??
+
+# spacing
+Span = 120
+D_fus = 9
+length_ailerons = 0.2* Span  # the aileron are 20 % of the total span
+min_spacing = 0.1
+spacing = (Span - D_fus - 2*length_ailerons -2*min_spacing - D_fan* n_fans)  / (n_fans-2)
+if 0<spacing< min_spacing :
+    n_fans_fit = (Span - D_fus - 2*length_ailerons)/(D_fan+min_spacing)
+    print ('The engines are too close together. There are', n_fans,'fans but only', n_fans_fit, 'fit in the wing')
+elif spacing<0 :
+    n_fans_fit = (Span - D_fus - 2*length_ailerons )/(D_fan+min_spacing)
+    print('The engines do not fit on the wing. There are', n_fans,'fans but only', n_fans_fit, 'fit in the wing')
 
 
-
-print ('radius of a fan', r)
-print ('number of fans', n_fans)
-print ('Minimum wing span (not accounting for fuselage) ', n_fans*2*r)
-print ('blade tip speed', omega*r)
+print ('Radius of a fan', r,'m')
+print ('Number of fans', n_fans)
+print ('Blade tip speed', omega*r, 'm/s')
+print ('Length of fan', length_fan, 'm')
+print ('Total weight', mass_total*10**(-3),'Ton')
+print ('Mass single fan', mass_fan,'kg')
+print ('Mass motor inverter', mass_motor_inverter,'kg')
+print('Spacing',spacing,'m')
