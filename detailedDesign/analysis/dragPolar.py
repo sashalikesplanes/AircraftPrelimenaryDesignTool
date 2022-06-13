07d34a07ce5633e8e906f5b9148976de24e24c20
mikad74 <mika.doorenbosch@gmail.com>
2022-06-13 11:02:24 +0200

    produce plot drag polar

--- detailedDesign/analysis/dragPolar.py
+++ detailedDesign/analysis/dragPolar.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 def make_drag_polar(aircraft):
-    alphas = np.linspace(-20,10, 100)
+    alphas = np.linspace(-15,10, 100)
     C_L_s = aircraft.WingGroup.Wing.get_C_L(alphas)
     C_D_s = aircraft.C_D_min + C_L_s ** 2 / (np.pi * aircraft.WingGroup.Wing.aspect_ratio * aircraft.WingGroup.Wing.oswald)
 
 
     fig, ax = plt.subplots()
     ax.plot(C_D_s, C_L_s, color='k')
-    ax.plot([min(C_D_s)-0.01, max(C_D_s)] ,[0,0], alpha=0)
+    ax.plot([min(C_D_s)-0.012, max(C_D_s)] ,[0,0], alpha=0)
 
     ax.spines['left'].set_position('zero')
 
@@ -26,6 +26,7 @@ def make_drag_polar(aircraft):
     ax.set_ylabel(r'$C_L$', fontsize=20)
     save_path = Path("plots", "drag_polar")
     plt.savefig(save_path, dpi=600)
+    # plt.show()
     plt.close()
 
 
