b25e07c95d304989950a0da20431d2e8c03c40d6
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-06-14 10:19:26 +0200

    idk

--- detailedDesign/flight_profile_diag.py
+++ detailedDesign/flight_profile_diag.py
@@ -1,3 +1,18 @@
 import numpy as np
+from dataclasses import dataclass
+
+takeoff_distance = 10 # km
+cruise_altitude = 3 # km
+
+@dataclass
+class ProfilePoint:
+    name: str
+    range_: int # km
+    altitude: int # km
+
+        # range km, altitude m
+points = [ProfilePoint('Engine Start', 0, 0),
+          ProfilePoint('Takeoff', 10, 0),
+          ProfilePoint('Climb to Cruise', 
 
 
